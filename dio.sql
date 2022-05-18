50a682b9b0785b8881fa31245ab040236426f385
Marillia Dantas <105676314+marilliadantas@users.noreply.github.com>
2022-05-18 19:08:39 -0300

    Update dio.sql

--- dio.sql
+++ dio.sql
@@ -8,3 +8,14 @@ INSERT INTO pessoas (nome, nascimento) VALUES ('Nathally', '1990 05 22')
 INSERT INTO pessoas (nome, nascimento) VALUES ('João', '1995 12 13')
 INSERT INTO pessoas (nome, nascimento) VALUES ('Marcela', '2000 04 08')
 
+UPDATE pessoas SET nome='Nathally Souza ' WHERE id= '1'
+
+UPDATE pessoa SET genero='M' WHERE id=2; 
+
+UPDATE pessoa SET genero='F' WHERE id=3;
+
+UPDATE pessoa SET genero='M' WHERE id=6; 
+
+
+
+SELECT COUNT(id), GENERO FROM pessoas GROUP BY genero
